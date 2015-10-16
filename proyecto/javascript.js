0506f9d06653497ddf332569be31cced3a44bdc5
alu0100706734 <alu0100706734@ull.edu.es>
2015-10-16 19:53:35 +0200

    nuevo prueba

--- proyecto/javascript.js
+++ proyecto/javascript.js
@@ -1,11 +1,55 @@
-var mic;
-function setup(){
-	p5.enabled;
+function preload(){
+	
+  var mic;
+  function setup(){
   mic = new p5.AudioIn()
   mic.start();
+  
+  sound = mic;
+  
+});
 }
+
+function setup(){
+  cnv = createCanvas(100,100);
+  sound.amp(0);
+  sound.loop();
+  fft = new p5.FFT();
+}
+
 function draw(){
   background(0);
-  micLevel = mic.getLevel();
-  ellipse(width/2, constrain(height-micLevel*height*5, 0, height), 10, 10);
+
+  var spectrum = fft.analyze(); 
+  noStroke();
+  fill(0,255,0); // spectrum is green
+  for (var i = 0; i< spectrum.length; i++){
+    var x = map(i, 0, spectrum.length, 0, width);
+    var h = -height + map(spectrum[i], 0, 255, height, 0);
+    rect(x, height, width / spectrum.length, h )
+  }
+
+  var waveform = fft.waveform();
+  noFill();
+  beginShape();
+  stroke(255,0,0); // waveform is red
+  strokeWeight(1);
+  for (var i = 0; i< waveform.length; i++){
+    var x = map(i, 0, waveform.length, 0, width);
+    var y = map( waveform[i], -1, 1, 0, height);
+    vertex(x,y);
+  }
+  endShape();
+
+  isMouseOverCanvas();
+}
+
+// fade sound if mouse is over canvas
+function isMouseOverCanvas() {
+  var mX = mouseX, mY = mouseY;
+  if (mX > 0 && mX < width && mY < height && mY > 0) {
+      sound.amp(0.5, 0.2);
+  } else {
+    sound.amp(0, 0.2);
+  }
 }
